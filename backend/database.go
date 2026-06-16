1169764c69617adcf57015c5b0e9bde7dd8ba855
dineshgadiraju <venkatdinesh5010@gmail.com>
2026-06-16 14:41:06 -0500

    Prepare backend for deployment

--- backend/database.go
+++ backend/database.go
@@ -3,24 +3,36 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-var DB *pgx.Conn
+var DB *pgxpool.Pool
 
 func ConnectDB() {
+	databaseURL := os.Getenv("DATABASE_URL")
+
+	if databaseURL == "" {
+		databaseURL = "postgres://postgres:welcome2ibm@localhost:5433/careercopilot?sslmode=disable"
+	}
+
 	var err error
 
-	DB, err = pgx.Connect(
+	DB, err = pgxpool.New(
 		context.Background(),
-		"postgres://postgres:welcome2ibm@localhost:5433/careercopilot?sslmode=disable",
+		databaseURL,
 	)
 
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("✅ Connected to PostgreSQL")
+	err = DB.Ping(context.Background())
 
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("✅ Connected to PostgreSQL")
 }
